17165af926bf91d55ec503d7d6e297e3824aed5e
vitor <vitorpaulo3121@gmail.com>
2022-07-25 17:59:53 -0300

    little changes on settings screend

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -15,8 +15,33 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(236, 236, 240, 1),
-      body: Row(
-        children: [],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Row(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width / 5,
+              child: Column(),
+            ),
+            Expanded(child: LayoutBuilder(
+              builder: (context, constraints) {
+                return Card(
+                  margin: EdgeInsets.all(0),
+                  elevation: 10,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        bottomLeft: Radius.circular(30)),
+                  ),
+                  child: Container(
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight,
+                  ),
+                );
+              },
+            ))
+          ],
+        ),
       ),
     );
   }
